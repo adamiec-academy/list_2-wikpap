5a39dd47251c4b5d050ccf17a3913d67f8c7e303
wikpap <115736945+wikpap@users.noreply.github.com>
2022-10-25 13:30:54 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,15 @@
 def remove_parentheses(text):
+    result = ""
+    is_inside = False
+
+    for letter in text:
+        if letter == "(":
+            result += ""
+        elif letter == ")":
+            result += ""
+        else:
+            result +=letter
+
     return text
+
+print(remove_parentheses("Ala (ma) kota"))
